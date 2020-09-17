67ae6d516e8c98215a60d42161023c1c071270df
cgriffin332 <cgriffin332@gmail.com>
2020-09-17 11:26:28 -0400

    created example ajax call that returns 5 random meals given the meal type of chicken.

--- assets/dinner.js
+++ assets/dinner.js
@@ -1,1 +1,26 @@
-console.log("hello world Dinner")
+$(document).ready(function () {
+  // different meal categories: Beef, Chicken, Lamb, Pork, Seafood, Goat, Vegetarian
+  //later set this to $("#foodChoice").val()
+  var mealType = "Chicken";
+  var mealURL =
+    "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + mealType;
+
+  $.ajax({
+    url: mealURL,
+    method: "GET",
+  }).then(function (mealResponse) {
+    console.log(mealResponse);
+    var num = mealResponse.meals.length;
+    var randomNums = [];
+    while (randomNums.length < 5) {
+      var newRandom = Math.floor(Math.random() * num);
+      if (!randomNums.includes(newRandom)) {
+        randomNums.push(newRandom);
+      }
+    }
+    console.log(randomNums);
+    for (var i = 0; i < 5; i++) {
+      console.log(mealResponse.meals[randomNums[i]]);
+    }
+  });
+});
