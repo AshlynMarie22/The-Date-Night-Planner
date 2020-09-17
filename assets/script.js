d395c9f3e834c0e171e227ae2247785529c4df9c
Ray Bray <rbray.bray2@gmail.com>
2020-09-17 14:31:55 -0400

    Made function called top movies that makes the ajax call, and can dynamically generate the elements on the page

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     console.log("Hello World")
 
     //Create Settings Variable to pass into Ajax Function
-    var genre = "";
+    var genre = "action";
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -14,34 +14,37 @@ $(document).ready(function () {
         }
     }
     
-    $.ajax(settings).done(function (response) {
-        for (var i = 0; i < 5; i++) {
-            console.log(response.results[i]);
-            console.log("Movie Title: " + response.results[i].title);
-            console.log("IMDB Rating: " + response.results[i].imdbrating);
-            console.log("Synopsis: " + response.results[i].synopsis);
-            console.log(response.results[0].imageurl[i]);
-    
-            //Display elements to page
-            //Create elements
-            // var movieDiv = $("<div>");
-            var movieName = $("<div>");
-            var movieRating = $("<div>");
-            var movieSynopsis = $("<div>");
-            var movieImage = $("<img>");
-    
-            //Add content
-            movieName.text("Movie Title: " + response.results[i].title)
-            movieRating.text("Movie Rating: " + response.results[i].imdbrating)
-            movieSynopsis.text("Movie Synopsis: " + response.results[i].synopsis)
-            movieImage.attr("src", "response.results[i].imageurl[i]");
-            console.log(movieImage);
-    
-            //Display to Page
-            $("#here").append(movieName);
-            $("#here").append(movieRating);
-            $("#here").append(movieSynopsis);
-            $("#here").append("<img src='" + response.results[i].imageurl[0] + "'/>");
-        }
-    });
+    function topMovies() {
+        $.ajax(settings).done(function (response) {
+            for (var i = 0; i < 5; i++) {
+                console.log(response.results[i]);
+                console.log("Movie Title: " + response.results[i].title);
+                console.log("IMDB Rating: " + response.results[i].imdbrating);
+                console.log("Synopsis: " + response.results[i].synopsis);
+                console.log(response.results[0].imageurl[i]);
+        
+                //Create elements
+                // var movieDiv = $("<div>");
+                var movieName = $("<div>");
+                var movieRating = $("<div>");
+                var movieSynopsis = $("<div>");
+                var movieImage = $("<img>");
+        
+                //Add content
+                movieName.text("Movie Title: " + response.results[i].title)
+                movieRating.text("Movie Rating: " + response.results[i].imdbrating)
+                movieSynopsis.text("Movie Synopsis: " + response.results[i].synopsis)
+                movieImage.attr("src", "response.results[i].imageurl[i]");
+                console.log(movieImage);
+        
+                //Display to Page
+                $("#here").append(movieName);
+                $("#here").append(movieRating);
+                $("#here").append(movieSynopsis);
+                $("#here").append("<img src='" + response.results[i].imageurl[0] + "'/>");
+            }
+        });
+    }
+
+
 });
