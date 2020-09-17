ef038a9e5f68a637285ec6933b9a2bc0a35d2252
cgriffin332 <cgriffin332@gmail.com>
2020-09-17 11:46:14 -0400

    added comments to explain everything.

--- assets/dinner.js
+++ assets/dinner.js
@@ -4,14 +4,17 @@ $(document).ready(function () {
   var mealType = "Chicken";
   var mealURL =
     "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + mealType;
-
+// ajax call to get 5 meals based on mealType
   $.ajax({
     url: mealURL,
     method: "GET",
   }).then(function (mealResponse) {
     console.log(mealResponse);
+    //the length of possible meals
     var num = mealResponse.meals.length;
+    //empty array to store random numbers
     var randomNums = [];
+    //create random array of 5 numbers
     while (randomNums.length < 5) {
       var newRandom = Math.floor(Math.random() * num);
       if (!randomNums.includes(newRandom)) {
@@ -19,8 +22,10 @@ $(document).ready(function () {
       }
     }
     console.log(randomNums);
+    // iterate through random number array to present us with 5 random meals
     for (var i = 0; i < 5; i++) {
       console.log(mealResponse.meals[randomNums[i]]);
     }
+    
   });
 });
